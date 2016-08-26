9b71559e2329f590b1a380ad7dee872bd514dbf5
zixiaoning <939055013@qq.com>
2016-08-26 15:20:55 +0800

    8月26轮番,特色介绍,模块的完成

--- js/main.js
+++ js/main.js
@@ -3,7 +3,59 @@
  * @Author: iceStone
  * @Date:   2015-12-12 10:59:26
  * @Last Modified by:   iceStone
- * @Last Modified time: 2015-12-12 11:01:38
+ * @Last Modified time: 2015-12-13 15:19:19
  */
 
 'use strict';
+
+$(function() {
+    // 当文档加载完成才会执行
+    /**
+     * 根据屏幕宽度的变化决定轮播图片应该展示什么
+     * @return {[type]} [description]
+     */
+    function resize() {
+        // 获取屏幕宽度
+        var windowWidth = $(window).width();
+        // 判断屏幕属于大还是小
+        var isSmallScreen = windowWidth < 768;
+        // 根据大小为界面上的每一张轮播图设置背景
+        // $('#main_ad > .carousel-inner > .item') // 获取到的是一个DOM数组（多个元素）
+        $('#main_ad > .carousel-inner > .item').each(function(i, item) {
+            // 因为拿到是DOM对象 需要转换
+            var $item = $(item);
+            // var imgSrc = $item.data(isSmallScreen ? 'image-xs' : 'image-lg');
+            var imgSrc =
+                isSmallScreen ? $item.data('image-xs') : $item.data('image-lg');
+
+            // jQuery方式
+            // $element.data()
+            // 是一个函数 ，专门用于取元素上的自定义属性（data-abc）
+            // 函数的参数是我们要取得属性名称（abc）
+            //
+            // $element.attr('data-abc')
+            //
+            // JS中的写法
+            // element.dataset['abc']
+            //
+            // element.getAttribute('data-abc')
+            // element.setAttribute('data-abc','')
+
+            // 设置背景图片
+            $item.css('backgroundImage', 'url("' + imgSrc + '")');
+            //
+            // 因为我们需要小图时 尺寸等比例变化，所以小图时我们使用img方式
+            if (isSmallScreen) {
+                $item.html('<img src="' + imgSrc + '" alt="" />');
+            } else {
+                $item.empty();
+            }
+        });
+    }
+    // $(window).on('resize', resize);
+    // // 让window对象立即触发一下resize
+    // $(window).trigger('resize');
+
+
+    $(window).on('resize', resize).trigger('resize');
+});
